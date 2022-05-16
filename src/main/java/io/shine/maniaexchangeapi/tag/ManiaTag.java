c6e73f92dbfc6d8e1644420d83c235a620c0a2d1
Shineeeeeuh <thomasbar59@gmail.com>
2022-05-16 15:12:25 +0200

    Working on getTags

--- src/main/java/io/shine/maniaexchangeapi/tag/ManiaTag.java
+++ src/main/java/io/shine/maniaexchangeapi/tag/ManiaTag.java
@@ -1,5 +1,13 @@
 package io.shine.maniaexchangeapi.tag;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public class ManiaTag {
+    @Getter
+    private String name,color;
+    @Getter
+    private int ID;
 
 }
--- src/main/java/io/shine/maniaexchangeapi/website/WebSite.java
+++ src/main/java/io/shine/maniaexchangeapi/website/WebSite.java
@@ -1,7 +1,24 @@
 package io.shine.maniaexchangeapi.website;
 
+import io.shine.maniaexchangeapi.tag.ManiaTag;
+import lombok.Getter;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class WebSite {
+    @Getter
+    private OkHttpClient client = new OkHttpClient().newBuilder().followRedirects(false).build();
 
     public abstract String getEndPoint();
-    
+
+    public List<ManiaTag> getTags() {
+        List<ManiaTag> tags = new ArrayList<>();
+        Request request = new Request.Builder().url(getEndPoint() + "/tags/gettags").build();
+        Call call = client.newCall(request);
+        return tags;
+    }
 }
