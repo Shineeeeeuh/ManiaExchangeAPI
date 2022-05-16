87bfae1faa95b10728e0e0a2c3964e5ad44a39a9
Shineeeeeuh <thomasbar59@gmail.com>
2022-05-16 15:27:56 +0200

    Adding MakeRequest function

--- src/main/java/io/shine/maniaexchangeapi/website/WebSite.java
+++ src/main/java/io/shine/maniaexchangeapi/website/WebSite.java
@@ -2,9 +2,8 @@ package io.shine.maniaexchangeapi.website;
 
 import io.shine.maniaexchangeapi.tag.ManiaTag;
 import lombok.Getter;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import lombok.SneakyThrows;
+import okhttp3.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +13,17 @@ public abstract class WebSite {
     private OkHttpClient client = new OkHttpClient().newBuilder().followRedirects(false).build();
 
     public abstract String getEndPoint();
-
+    
     public List<ManiaTag> getTags() {
         List<ManiaTag> tags = new ArrayList<>();
-        Request request = new Request.Builder().url(getEndPoint() + "/tags/gettags").build();
-        Call call = client.newCall(request);
         return tags;
+    }
+
+    @SneakyThrows
+    public Response makeRequest(String path, RequestBody body){
+        Request.Builder builder = new Request.Builder().url(getEndPoint()+path);
+        if(body != null) builder.post(body);
+        return client.newCall(builder.build()).execute();
+
     }
 }
