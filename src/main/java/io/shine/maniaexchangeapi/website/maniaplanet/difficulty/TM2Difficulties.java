bb2b03c651a08c5723bf370ab80684eb93287065
Shineeeeeuh <thomasbar59@gmail.com>
2022-05-17 16:46:59 +0200

    Adding getDifficulties

--- src/main/java/io/shine/maniaexchangeapi/website/maniaplanet/difficulty/TM2Difficulties.java
+++ src/main/java/io/shine/maniaexchangeapi/website/maniaplanet/difficulty/TM2Difficulties.java
@@ -3,6 +3,9 @@ package io.shine.maniaexchangeapi.website.maniaplanet.difficulty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @AllArgsConstructor
 public enum TM2Difficulties {
     Beginner("Beginner", 0),
@@ -16,4 +19,8 @@ public enum TM2Difficulties {
     private String name;
     @Getter
     private int value;
+
+    public static Optional<TM2Difficulties> getDifficulties(int id){
+        return Arrays.stream(values()).filter(d -> d.getValue() == id).findFirst();
+    }
 }
