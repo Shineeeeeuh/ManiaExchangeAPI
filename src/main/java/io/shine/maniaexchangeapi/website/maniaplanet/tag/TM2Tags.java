80e96f4a05e12f492a0e6eb13dff8a30c1efc304
Shineeeeeuh <thomasbar59@gmail.com>
2022-05-17 14:11:29 +0200

    Adding Constructor to TM2Tags

--- src/main/java/io/shine/maniaexchangeapi/website/maniaplanet/tag/TM2Tags.java
+++ src/main/java/io/shine/maniaexchangeapi/website/maniaplanet/tag/TM2Tags.java
@@ -1,5 +1,15 @@
 package io.shine.maniaexchangeapi.website.maniaplanet.tag;
 
-public enum TM2Tags {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+public enum TM2Tags {
+    //BASED ON https://tm.mania.exchange/api/tags/gettags
+
+    Race(1, "Race", "");
+    @Getter
+    private int id;
+    @Getter
+    private String name, color;
 }
