28b6d604936e98a1de51691b580946f1d06dee61
Shineeeeeuh <thomasbar59@gmail.com>
2022-06-20 09:29:11 +0200

    Add tm2echange to maniaexchangeapi

--- src/main/java/io/shine/maniaexchangeapi/ManiaExchangeAPI.java
+++ src/main/java/io/shine/maniaexchangeapi/ManiaExchangeAPI.java
@@ -1,5 +1,9 @@
 package io.shine.maniaexchangeapi;
 
-public class ManiaExchangeAPI {
+import io.shine.maniaexchangeapi.website.maniaplanet.TM2Exchange;
+import lombok.Getter;
 
+@Getter
+public class ManiaExchangeAPI {
+    private final TM2Exchange TM2Exchange = new TM2Exchange();
 }
